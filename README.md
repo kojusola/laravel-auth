ae48b635f4039b626ea13d8793745823ec33b9bd
kojusola <adeola5678@gmail.com>
2021-06-13 22:32:39 +0100

    auth

---
+++ README.md
@@ -0,0 +1,9 @@
+A simple user Authentication built with laravel.
+
+It has the following endpoints:
+1. Register User
+2. Login User
+3. Logout User
+4. User details
+5. Edit user details
+6. Delete user Details.
---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register (Request $request)
+    {
+
+        return User::create([
+            'name'=> $request->input('name'),
+            'email'=> $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+    }
+
+    public function login (Request $request)
+    {
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response([
+                'message' => 'Invalid credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+
+        $token =$user -> createToken('token')->plainTextToken;
+        
+        $cookie = cookie('jwt', $token, 60*24);
+
+        return response([
+            'message' => $token
+        ])->withCookie($cookie);
+    }
+
+    public function user()
+    {
+        return Auth::user();
+    }
+
+    public function update (Request $request)
+    {
+        $user = Auth::user();
+        $update = $request->all();
+        $user->fill($update)->save();
+        return response([
+            'message'=>'success'
+        ]);
+    }
+
+    public function delete (Request $request)
+    {
+        $user = Auth::user();
+        $user -> delete();
+        $cookie = Cookie::forget('jwt');
+        return response([
+            'message'=>'success'
+        ])->withCookie($cookie);
+    }
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('jwt');
+        
+        return response([
+            'message'=>'success'
+        ])->withCookie($cookie);
+    }
+}
